97476a072d664cdf9f94f3723d5a96c9c5a11a89
Ravshan <ravkart@gmail.com>
2019-07-09 16:06:30 -0700

    deployment

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.png';
+//import logo from './logo.png';
 
 const Logo = () => {
   return (
 		<div className='ma4 mt0'>   
 	      <div className="pa3" style={{ height: 200, width: 200 }}>
-	        <img style={{paddingTop: '5px'}} alt='logo' src={logo}/>
+	        <img style={{paddingTop: '5px'}} alt='logo' src=''/>
 	      </div>
 		</div>
   );
