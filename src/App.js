0c549d5f33c80fd11e1ae9e70edf53abeb99efbd
Ravshan <ravkart@gmail.com>
2019-07-10 00:15:06 -0700

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ onButtonSubmit = () => {
     .then(response => response.json())
     .then(response => {
         if (response) {
-          fetch('https://warm-forest-93262.herokuapp.com:3000/image', {
+          fetch('https://warm-forest-93262.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
