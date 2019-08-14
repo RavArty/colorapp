794a4810b36eb7f48bb709913d80db3725d14132
Ravshan <ravkart@gmail.com>
2019-08-13 19:57:33 -0700

    Updates

---
+++ precache-manifest.c923dfb3566bdadcb601b2d8be8a6600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d454dc999cefc60367f08a1df71a029e",
+    "url": "/colorapp/index.html"
+  },
+  {
+    "revision": "ac87c16aaa4aaa2e500f",
+    "url": "/colorapp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "309235bc7a2f652e1303",
+    "url": "/colorapp/static/css/main.0c9b671c.chunk.css"
+  },
+  {
+    "revision": "ac87c16aaa4aaa2e500f",
+    "url": "/colorapp/static/js/2.1fbb4cc0.chunk.js"
+  },
+  {
+    "revision": "309235bc7a2f652e1303",
+    "url": "/colorapp/static/js/main.452a6b98.chunk.js"
+  },
+  {
+    "revision": "a39cebc99dc6f7d3291d",
+    "url": "/colorapp/static/js/runtime~main.a94f96ec.js"
+  }
+]);
