fbb65b15a86ab0d490610b4ab4431274766b817a
Ravshan <ravkart@gmail.com>
2019-08-18 23:54:34 -0700

    added persist to redux to save data in a storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import imageReducer from './image/image.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['image', 'user']
+};
+
+const rootReducer =  combineReducers({
   user: userReducer,
   image: imageReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
